d6a3572655844028863fee3d524d8612c08173b7
angelum23 <angelojosedarosa01@hotmail.com>
2022-06-05 20:18:54 -0300

    alteracoes relativas a criacao do metodo post com erro de conversao do arquivo json

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
-def montaGrafico():
-    return jsonify({"Text": 'Hello World!',
-                    "Text2": 'Hello World!'})
+
+@app.route('/montaGrafico', methods=['GET', 'POST'])
+def monta_grafico():
+    if request.method == 'GET':
+        return jsonify({'response': 'Get request called'})
+    elif request.method == 'POST':
+        req_json = request.json
+        formula = req_json['formula']
+        return jsonify({'text': formula})
+
 
 app.run(debug=True)
