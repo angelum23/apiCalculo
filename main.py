98fd1ece8a84550896bca3de9bb90552b82cbf9b
angelum23 <angelojosedarosa01@hotmail.com>
2022-06-06 13:08:19 -0300

    feito as requisicoes funcionarem e adicionado retorno da lista

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ def monta_grafico():
     if request.method == 'GET':
         return jsonify({'response': 'Get request called'})
     elif request.method == 'POST':
-        req_json = request.json
-        formula = req_json['formula']
-        return jsonify({'text': formula})
+        formula = str(request.args.get('formula'))
+        list_results = list()
+        for i in range(-10, 10):
+            f1 = formula
+            f1 = f1.replace('x²', str(i)+'*'+str(i))
+            f1 = f1.replace('x', str(i))
+            list_results.append(f1)
+        return jsonify({'listFunc': list_results})
 
 
 app.run(debug=True)
